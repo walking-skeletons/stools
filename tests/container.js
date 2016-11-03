9ae37446c1499e208351524d97251bebe1f99261
k33g <k33g@github.com>
2016-11-03 15:17:36 +0100

    :panda_face: Add Container + tests

---
+++ tests/container.js
@@ -0,0 +1,20 @@
+"use strict";
+const assert = require('chai').assert;
+
+const Container = require('../src/Container.js');
+
+console.log(Container)
+
+describe("container value equals 42", () => {
+  let container = Container.of(42)
+  it("should return 42", () => {
+    assert.equal(container.value, 432)
+  });
+});
+
+describe("container value equals 'Bob Morane'", () => {
+  let bob = Container.of('Bob Morane')
+  it("should return 'Bob Morane'", () => {
+    assert.equal(bob.value, 'Bob Morane')
+  });
+});
